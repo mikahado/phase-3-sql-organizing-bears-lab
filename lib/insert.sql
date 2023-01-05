406ad226bdfdb9d8ab61e1de6d453b53797a31af
Michael <mikahado@gmail.com>
2023-01-04 19:35:38 -0500

    completes lab tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 12, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES(2, "Rowdy", 20, "F", "brown", "dumb", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 33, "F", "black", "happy", 1);
+INSERT INTO bears VALUES (4, "Sargeant Brown", "M", 2, "blue", "quiet", 1);
+INSERT INTO bears VALUES (5, "Melissa", 13, "M", "grizzy", "psycho", 1);
+INSERT INTO bears VALUES (6,"Grinch", 33, "F", "blac", "hppy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 2, "M", "ble", "quet", 1);
+INSERT INTO bears VALUES (8, null, 13,  "M", "grzzy","pyco", 0);
